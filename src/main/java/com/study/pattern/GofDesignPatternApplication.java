9edb92cb6e32669a87539ef6248632ab162860a5
wonpyol <wonpyol@gmail.com>
2023-03-02 20:43:10 +0900

    first commit

---
+++ src/main/java/com/study/pattern/GofDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.study.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GofDesignPatternApplication.class, args);
+    }
+
+}
